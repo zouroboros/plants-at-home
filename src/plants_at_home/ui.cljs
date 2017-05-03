0cc07ad85a7dc6381a4421af93464c2693aa01eb
Zouroboros <zouroboros@gmail.com>
2017-05-03 10:35:19 +0200

    Added plant search.

--- src/plants_at_home/ui.cljs
+++ src/plants_at_home/ui.cljs
@@ -2,17 +2,41 @@
   (:require-macros [cljs.core.async.macros :refer [go]])
   (:require [cljs.core.async :refer [put! chan <!]]
             [om.core :as om :include-macros true]
-            [om.dom :as dom :include-macros true]))
+            [om.dom :as dom :include-macros true]
+            [clojure.string :as string]))
 
 (def app-state (atom {:plants nil :current-plant nil}))
 
 (defn plant-list-item [current-plant plant]
   (let [class (if (= current-plant plant) "current" nil)]
-    (om/component (dom/li nil (dom/a #js {:href (str "#" plant) :className class} plant)))))
+    (om/component (dom/li nil
+      (dom/a #js {:href (str "#" plant) :className class} plant)))))
 
-(defn plant-list [current-plant plants]
-      (om/component (dom/div nil (dom/h1 nil "Plants")
-        (apply dom/ul #js {:className "plant-list"} (om/build-all (partial plant-list-item current-plant) plants)))))
+(defn plant-search [data owner]
+  (reify om/IRenderState
+    (render-state [this {:keys [search]}]
+      (dom/div #js {:className "search-box"}
+        (dom/input #js {:type "text" :onChange (fn [e] (put! search (.. e -target -value)))})))))
+
+(defn plant-list [data owner]
+  (reify
+    om/IInitState
+      (init-state [_]
+        {:search-c (chan) :current-plants (:plants data)})
+    om/IWillMount
+      (will-mount [_]
+        (let [search-c (om/get-state owner :search-c)]
+        (go (loop []
+             (let [search (<! search-c)]
+               (om/set-state! owner :current-plants (filter (fn [plant] (string/includes? plant search)) (:plants data)))
+               (recur))))))
+     om/IRenderState
+     (render-state [this {:keys [search-c current-plants]}]
+       (dom/div nil
+       (om/build plant-search () {:init-state {:search search-c}})
+       (apply dom/ul #js {:className "plant-list"}
+         (om/build-all (partial plant-list-item (:current-plant data)) current-plants))))))
+
 
 (defn wait-view []
   (reify
@@ -20,14 +44,19 @@
     (render [this]
       (dom/h1 nil "Loading data"))))
 
-(defn read-url-hash [channel] (let [plant (js/decodeURIComponent (subs js/document.location.hash 1))] (put! channel plant)))
+(defn read-url-hash [channel]
+  (let [plant (js/decodeURIComponent (subs js/document.location.hash 1))]
+    (put! channel plant)))
 
 (defn init-ui [ui-channel]
   (om/root
     (fn [data owner]
       (reify om/IRender
-        (render [_] (if (nil? (:plants data)) (om/build wait-view nil) (om/build (partial plant-list (:current-plant data)) (:plants data))))))
+        (render [_] (if (nil? (:plants data)) (om/build wait-view nil)
+          (om/build
+            plant-list data)))))
     app-state
     {:target (. js/document (getElementById "app"))})
-  (. js/window (addEventListener "hashchange" (fn [e] (read-url-hash ui-channel))))
+  (. js/window (addEventListener "hashchange"
+    (fn [e] (read-url-hash ui-channel))))
   (if (nil? js.document.location.hash) () (read-url-hash ui-channel)))
